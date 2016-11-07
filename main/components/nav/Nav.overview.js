5f6b7e947ed478768f105d875fe77d5cf38d21e8
Keith Wallace <keith.t.wallace@gmail.com>
2016-11-07 12:40:05 -0500

    Added toggle tab functionality for Overview area

--- main/components/nav/Nav.overview.js
+++ main/components/nav/Nav.overview.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// import toggleOverviewNav from '../../lib/toggleOverviewNav'
 
 export default class Overview extends React.Component {
   constructor(props){
@@ -8,16 +9,34 @@ export default class Overview extends React.Component {
     }
   }
 
+  toggleOverviewNav(event) {
+    let Attendees = document.getElementById('nav-attendees');
+    let Activities = document.getElementById('nav-activities');
+    let AttendeesLI = document.getElementById('nav-li-attendees');
+    let ActivitiesLI = document.getElementById('nav-li-activities');
+
+    if((event.currentTarget.id === 'nav-li-attendees') && (!Attendees.classList.contains('show-tab'))) {
+      Attendees.classList.add('show-tab');
+      AttendeesLI.classList.add('icon-tri-down');
+      Activities.classList.remove('show-tab');
+      ActivitiesLI.classList.remove('icon-tri-down');
+    } else if((event.currentTarget.id === 'nav-li-activities') && (!Activities.classList.contains('show-tab'))) {
+      Activities.classList.add('show-tab');
+      ActivitiesLI.classList.add('icon-tri-down');
+      Attendees.classList.remove('show-tab');
+      AttendeesLI.classList.remove('icon-tri-down');
+    }
+  }
+
   render() {
     return (
       <nav className="overview-nav">
         <div className="container overview-nav-area">
           <ul>
-            <li id="nav-li-attendees">
+            <li id="nav-li-attendees" className="icon-tri-down" onClick={this.toggleOverviewNav}>
               Attendees
-              <img src="../../../assets/icon-triangle-down-white.svg" />
             </li>
-            <li id="nav-li-activities">
+            <li id="nav-li-activities" onClick={this.toggleOverviewNav}>
               Activities ({this.props.activityCount})
             </li>
           </ul>
